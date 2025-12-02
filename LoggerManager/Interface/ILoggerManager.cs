d5d1d94e5fa7db3de8bcb00f6c06c5150773d5f4
Hasan Hüseyin KADI <160792141+mrkad61@users.noreply.github.com>
2025-12-02 15:40:34 +0300

    Logging işlemlerine giriş.

---
+++ LoggerManager/Interface/ILoggerManager.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggerManager.Interface;
+
+public interface ILoggerManager
+{
+    void LogInfo(string message);
+    void LogError(string message);
+    void LogWarn(string message);
+    void LogDebug(string message);
+}
